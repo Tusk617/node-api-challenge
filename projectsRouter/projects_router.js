4d6a909fb4d8628a73dc1fb55ce7009326700e67
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-05 11:51:55 -0400

    Got get, get by id, and post for the projects router CRUD operations working.

--- projectsRouter/projects_router.js
+++ projectsRouter/projects_router.js
@@ -4,9 +4,23 @@ const Projects = require("../data/helpers/projectModel.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Projects.get(req.query)
+    Projects.get(req.parms)
     .then(projects => {
         res.status(200).json(projects)
+    })
+})
+
+router.get("/:id", (req, res) => {
+    Projects.get(req.params.id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+})
+
+router.post("/", (req, res) => {
+    Projects.insert(req.body)
+    .then(newPost => {
+        res.status(200).json(newPost)
     })
 })
 
