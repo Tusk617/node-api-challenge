8328c7f4dbd5591daf17817146b246b8febc0ef7
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-05 11:58:58 -0400

    Got the delete request working, might add some more descriptivness to its response if time allows

--- projectsRouter/projects_router.js
+++ projectsRouter/projects_router.js
@@ -8,21 +8,32 @@ router.get("/", (req, res) => {
     .then(projects => {
         res.status(200).json(projects)
     })
-})
+}) //working
 
 router.get("/:id", (req, res) => {
     Projects.get(req.params.id)
     .then(project => {
         res.status(200).json(project)
     })
-})
+}) //working
 
 router.post("/", (req, res) => {
     Projects.insert(req.body)
     .then(newPost => {
         res.status(200).json(newPost)
     })
-})
+}) //working
+
+router.delete("/:id", (req, res) => {
+    const postId = req.params.id
+    Projects.get(postId)
+    .then(project => {
+        Projects.remove(postId)
+    .then(deletedProject => {
+        res.status(202).json(`Number of deleted projects: ${deletedProject}`)
+    })
+    })
+}) //working
 
 
 module.exports = router;
